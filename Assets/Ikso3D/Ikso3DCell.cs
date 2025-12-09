d63bda798f0985d6e75cc1d873b7284957e2f142
SummerGuardian64 <summerg64-gh@email1.me>
2025-12-09 03:46:20 +0100

    Sunny branch - Cumulative work for Monday (#12)
    * Changes to the TV
    
    * Ikso3D started
    
    Added Ikso3D (3D Tic-Tac-Toe)
    
    * Added PivotDoctor
    
    This script fixes pivot point issues
    
    * Cumulative work for Monday

---
+++ Assets/Ikso3D/Ikso3DCell.cs
@@ -0,0 +1,18 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.Udon;
+
+public class Ikso3DCell : UdonSharpBehaviour
+{
+    public Ikso3DBoard board;
+    public int cellIndex;
+
+    // Called by a pointer when it hits + clicks this cell
+    public void OnPointerClick(int playerColorIndex)
+    {
+        if (board != null)
+        {
+            board.ClickCell(cellIndex, playerColorIndex);
+        }
+    }
+}
